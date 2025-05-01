2655f7731641b6161f7af3a7fd48cda0052c3990
Tunere Ereibor <etunere@gmail.com>
2025-05-01 23:15:34 +0100

    done with set up

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View  className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl font-extrabold text-pink-300">Welcome</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl font-extrabold text-blue-400">
+        Hello World!
+      </Text>
     </View>
   );
 }
