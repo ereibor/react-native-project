0cd7a978efd665cadccb4a6d346d3ccd0b04c563
Tunere Ereibor <etunere@gmail.com>
2025-05-02 08:01:20 +0100

    test android studio

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-5xl font-extrabold text-blue-400">
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-6xl font-extrabold text-purple-500">
         Hello World!
       </Text>
     </View>
