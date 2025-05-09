c6e18dbb332cd350d66976c100e7f2337ec472ff
Tunere Ereibor <etunere@gmail.com>
2025-05-09 17:31:19 +0100

    finish extending colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
         primary: '#030014',
         secondary: '#151312',
         light: {
-          100: '#D6C6FF'
+          100: '#D6C6FF',
+          200: '#A8B5DB',
+          300: '#9CA4AB',
+        },
+        dark: {
+          100: '#221f3d',
+          200: '#0f0d23',
+          300: '#030014',
         },
         accent: '#AB8BFF'
       }
